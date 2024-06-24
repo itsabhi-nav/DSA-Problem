9d83bee6bb590d25095c1901a63e1732bc078c9d
Abhinav Dubey <118627945+itsabhi-nav@users.noreply.github.com>
2024-06-24 15:45:17 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (60.11%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+          int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        if (n == 2) return max(nums[0], nums[1]);
+        
+        // Initialize the dp array
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        
+        // Fill the dp array using the recurrence relation
+        for (int i = 2; i < n; ++i) {
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+        }
+        
+        return dp[n-1];
+    }
+};
